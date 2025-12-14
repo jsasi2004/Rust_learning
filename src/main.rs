3346bac6f2d37a9a703fc5771a2e19e1b69f9268
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-14 19:03:48 +0000

    Hello world

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    print!("Hello World!\n"); // Single line
+    println!("I am learning Rust.");
+    println!("It is awesome!");
+}
