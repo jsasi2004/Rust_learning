00d83d682a2ae38b7ab3e1e3a9b806b487b002f8
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-14 19:08:11 +0000

    variables and mut keyword

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    /* The code below will print the words Hello World!
-    to the screen, and it is amazing */
-    print!("Hello World!\n"); // Single line
-    println!("I am learning Rust.");
-    println!("It is awesome!");
+    let name = "John";
+    let age = 30;
+    println!("{} is {} years old.", age, name);
+
+    let mut x = 5;
+    print!("{}",x);
+    x=9;
+    print!("{}",x);
+    println!();
 }
