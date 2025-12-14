9dce0c196f64d87fbc8ffbf6938ef2f6ab55793d
Sashank Jonnadula <jsasi2804@gmail.com>
2025-12-14 19:27:49 +0000

    data types and variables

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::mem;
 fn main() {
-    let name = "John";
-    let age = 30;
-    println!("{} is {} years old.", age, name);
-
-    let mut x = 5;
-    print!("{}",x);
-    x=9;
-    print!("{}",x);
-    println!();
+    let _string = "hello world"; // unused so _ at the beginning
+    let _my_num: i32 = 5;          // integer
+    let _my_double: f64 = 5.99;    // float
+    let _my_letter: char = 'D';    // character
+    let _my_bool: bool = true;     // boolean
+    let _my_text: &str = "Hello";  // string
+    let my_num_64: i64 = 8;
+    println!("{}",my_num_64);
+    println!("{}",_my_bool);
+    println!("{}",mem::size_of_val(&_my_double));
+    println!("{}",mem::size_of::<i32>());
 }
